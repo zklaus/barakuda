bcdc93e30f487d4db3f39bf1d37e56cd1423b11e
Laurent Brodeau <brodeau@gmail.com>
2017-03-19 22:01:18 +0100

    LB: adding wind stress module to cdfcurl.f90

--- cdftools_light/src/cdfcurl.f90
+++ cdftools_light/src/cdfcurl.f90
@@ -6,7 +6,6 @@ PROGRAM cdfcurl
    !!
    !! history :
    !!   Original :  J.M. Molines (May 2005)
-   !!   Modified :  P.   Mathiot (June 2007) update for forcing fields
    !!---------------------------------------------------------------------
    !!  $Rev: 256 $
    !!  $Date: 2009-07-21 17:49:27 +0200 (Tue, 21 Jul 2009) $
@@ -20,17 +19,15 @@ PROGRAM cdfcurl
    INTEGER :: ji,jj,jk, jt, ilev, istatus
    INTEGER :: npiglo, npjglo, npk, nt
    INTEGER :: narg, iargc, ncout, ierr
-   INTEGER, DIMENSION(1) ::  ipk, id_varout         !
+   INTEGER, DIMENSION(2) ::  ipk, id_varout         !lolo
 
    REAL(kind=8), DIMENSION(:,:), ALLOCATABLE  :: e2v, e1u, e1f, e2f
-   REAL(kind=4), DIMENSION(:,:), ALLOCATABLE  :: un, vn, rotn, fmask, zun, zvn
+   REAL(kind=4), DIMENSION(:,:), ALLOCATABLE  :: un_t, vn_t, rotn, taum, tmask, fmask, zun, zvn
    REAL(KIND=4) ,DIMENSION(:), ALLOCATABLE    ::  tim
-
-   LOGICAL :: lforcing    = .FALSE.
 
    CHARACTER(LEN=256) :: cfilu, cfilv, cvaru, cvarv, cdum, ctim
    CHARACTER(LEN=256) :: cf_mm='mesh_mask.nc', cfileout='curl.nc'
-   TYPE (variable), DIMENSION(1) :: typvar         !: structure for attibutes
+   TYPE (variable), DIMENSION(2) :: typvar         !: structure for attibutes
 
    !!
    narg = iargc()
@@ -40,9 +37,6 @@ PROGRAM cdfcurl
       PRINT *,'        Produce a cdf file curl.nc with socurl variable'
       PRINT *,'        Need mesh_hgr.nc'
       PRINT *,'                         '
-      PRINT *,'        For forcing fields : '
-      PRINT *,'        if no z dimension put lev=0 (PM)'
-      PRINT *,'        if file is in grid B or C, check the code (PM)'
       STOP
    ENDIF
 
@@ -56,7 +50,7 @@ PROGRAM cdfcurl
    ! define new variables for output ( must update att.txt)
    typvar(1)%name='socurl'
    typvar(1)%units='10^-6 s-1'
-   typvar(1)%missing_value=0.
+   typvar(1)%missing_value=-9999.
    typvar(1)%valid_min= -1000.
    typvar(1)%valid_max= 1000.
    typvar(1)%long_name='Relative_Vorticity (curl)'
@@ -64,7 +58,18 @@ PROGRAM cdfcurl
    typvar(1)%online_operation='N/A'
    typvar(1)%axis='TYX'
 
-   ipk(1) = 1  !  2D
+   typvar(2)%name='sotaum'
+   typvar(2)%units='N/m^2'
+   typvar(2)%missing_value=-9999.
+   typvar(2)%valid_min= -1000.
+   typvar(2)%valid_max= 1000.
+   typvar(2)%long_name='Wind stress module (on T-point)'
+   typvar(2)%short_name='sotaum'
+   typvar(2)%online_operation='N/A'
+   typvar(2)%axis='TYX'
+
+
+   ipk(:) = 1  !  2D
 
    npiglo = getdim(cfilu,'x')
    npjglo = getdim(cfilu,'y')
@@ -87,7 +92,7 @@ PROGRAM cdfcurl
 
    ! if forcing field (PM)
    IF (ilev==0 .AND. npk==0) THEN
-      lforcing=.true.
+      !lforcing=.TRUE.
       npk = 1 ; ilev=1
       PRINT *, 'npk =0, assume 1'
    END IF
@@ -102,16 +107,17 @@ PROGRAM cdfcurl
 
    ! create output fileset
    ncout =create(cfileout, cfilu, npiglo,npjglo,0)
-   ierr= createvar(ncout ,typvar,1, ipk,id_varout )
+   ierr= createvar(ncout ,typvar, 2, ipk, id_varout )
    ierr= putheadervar(ncout, cfilu, npiglo, npjglo, 0)
 
 
    ! Allocate the memory
    ALLOCATE ( e1u(npiglo,npjglo) , e1f(npiglo,npjglo) )
    ALLOCATE ( e2v(npiglo,npjglo) , e2f(npiglo,npjglo) )
-   ALLOCATE ( un(npiglo,npjglo)  , vn(npiglo,npjglo)  )
+   ALLOCATE ( un_t(npiglo,npjglo)  , vn_t(npiglo,npjglo)  )
    ALLOCATE ( zun(npiglo,npjglo) , zvn(npiglo,npjglo) )
-   ALLOCATE ( rotn(npiglo,npjglo) , fmask(npiglo,npjglo) )
+   ALLOCATE ( rotn(npiglo,npjglo) , taum(npiglo,npjglo) )
+   ALLOCATE ( tmask(npiglo,npjglo) , fmask(npiglo,npjglo) )
    ALLOCATE ( tim(nt) )
 
    e1u=  getvar(cf_mm, 'e1u', 1,npiglo,npjglo)
@@ -119,6 +125,7 @@ PROGRAM cdfcurl
    e2v=  getvar(cf_mm, 'e2v', 1,npiglo,npjglo)
    e2f=  getvar(cf_mm, 'e2f', 1,npiglo,npjglo)
    fmask =  getvar(cf_mm, 'fmask', 1,npiglo,npjglo) ; !lolo
+   tmask =  getvar(cf_mm, 'tmask', 1,npiglo,npjglo) ; !lolo
 
 
    tim=getvar1d(cfilu,trim(ctim),nt)
@@ -132,34 +139,39 @@ PROGRAM cdfcurl
       jk = ilev
       zun(:,:) =  getvar(cfilu, cvaru, jk ,npiglo,npjglo, ktime=jt)
       zvn(:,:) =  getvar(cfilv, cvarv, jk ,npiglo,npjglo, ktime=jt)
-      IF ( lforcing ) THEN ! for forcing file u and v are on the A grid
-         DO ji=1, npiglo-1
-            un(ji,:) = 0.5*(zun(ji,:) + zun(ji+1,:))
-         END DO
-         !
-         DO jj=1, npjglo-1
-            vn(:,jj) = 0.5*(zvn(:,jj) + zvn(:,jj+1))
-         END DO
-         ! end compute u and v on U and V point
-      ELSE
-         un(:,:) = zun(:,:)
-         vn(:,:) = zvn(:,:)
-      END IF
 
+      !! VU and VV on T-point:
+      un_t = 0.
+      DO ji=2, npiglo
+         un_t(ji,:) = 0.5*(zun(ji-1,:) + zun(ji,:))*tmask(ji,:)
+      END DO
+      vn_t = 0.
+      DO jj=2, npjglo
+         vn_t(:,jj) = 0.5*(zvn(:,jj-1) + zvn(:,jj))*tmask(:,jj)
+      END DO
+      
       rotn(:,:) = 0.
       DO jj = 1, npjglo -1
          DO ji = 1, npiglo -1   ! vector opt.
-            rotn(ji,jj) = 1.E6 * (  e2v(ji+1,jj  ) * vn(ji+1,jj  ) - e2v(ji,jj) * vn(ji,jj)    &
-               &                   - e1u(ji  ,jj+1) * un(ji  ,jj+1) + e1u(ji,jj) * un(ji,jj)  ) &
+            rotn(ji,jj) = 1.E6 * (  e2v(ji+1,jj  ) * zvn(ji+1,jj  ) - e2v(ji,jj) * zvn(ji,jj)    &
+               &                   - e1u(ji  ,jj+1) * zun(ji  ,jj+1) + e1u(ji,jj) * zun(ji,jj)  ) &
                &           * fmask(ji,jj) / ( e1f(ji,jj) * e2f(ji,jj) )
          END DO
       END DO
       !
       rotn(npiglo,:) = rotn(2,:) ; ! periodicity lolo
-      
+      WHERE ( fmask == 0. ) rotn = -9999.
+
       ! write rotn on file at level k and at time jt
-      ierr = putvar(ncout, id_varout(1) ,rotn, 1 ,npiglo, npjglo, jt)
+      ierr = putvar(ncout, id_varout(1), rotn, 1 ,npiglo, npjglo, jt)
       
+      !! Wind stress module at T-point
+      taum(:,:) = 0.
+      taum(:,:) = SQRT( un_t(:,:)*un_t(:,:) + vn_t(:,:)*vn_t(:,:) )
+      taum(1,:) = taum(npiglo-1,:) ; ! periodicity lolo
+      WHERE ( tmask == 0. ) taum = -9999.
+      ierr = putvar(ncout, id_varout(2), taum, 1 ,npiglo, npjglo, jt)
+
    END DO
    ierr = closeout(ncout)
    
