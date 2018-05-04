7bc39e6698dac7b70744e48f21f648d05bb38ef8
brodeau <brodeau@gmail.com>
2018-05-04 17:48:25 +0200

    LB

--- python/exec/movie_nemo_zoom_box.py
+++ python/exec/movie_nemo_zoom_box.py
@@ -31,12 +31,13 @@ import barakuda_colmap as bcm
 import barakuda_tool as bt
 
 
-CNEMO = 'NATL60'
+#CNEMO = 'NATL60'
+CNEMO = 'NANUK025'
 
 #color_top = 'white'
 color_top = 'k'
 
-l_do_ice = False
+l_do_ice = True
 
 year_ref_ini = 2013
 
@@ -49,23 +50,41 @@ jt0 = 0
 i2=0
 j2=0
 
-#cbox = 'Biscay' ; i1 = 2880 ; j1 = 1060 ; rfact_zoom = 1. ; ny_res = 1080 ; nx_res = 1920
-#cbox = 'NAtlt' ; i1 = 0 ; j1 = 150 ; rfact_zoom = 0.36  ; ny_res = 1080 ; nx_res = 1920
 
-cbox = 'Med+NorthSea' ; i1 = 4086 ; j1 = 603 ; i2 = 5421 ; j2 = 3119 ; rfact_zoom = 0.3319
+if CNEMO == 'NATL60':
+    #cbox = 'Biscay' ; i1 = 2880 ; j1 = 1060 ; rfact_zoom = 1. ; ny_res = 1080 ; nx_res = 1920 ; vcb = [0.01, 0.08, 0.98, 0.025]
+    cbox = 'NAtlt' ; i1 = 0 ; j1 = 150 ; rfact_zoom = 0.36  ; ny_res = 1080 ; nx_res = 1920 ; vcb = [0.01, 0.08, 0.98, 0.025]
+    #cbox = 'Med+NorthSea' ; i1 = 4086 ; j1 = 603 ; i2 = 5421 ; j2 = 3119 ; rfact_zoom = 0.3319 ; vcb = [0.01, 0.08, 0.98, 0.025]
 
-if i2>0 and j2>0:
-    nx_res = i2-i1
-    ny_res = j2-j1
-else:
-    i2 = i1 + nxr
-    j2 = j1 + nyr
 
+if CNEMO == 'NANUK025':
+    cbox = 'ALL' ; i1 = 0 ; j1 = 0 ; i2 = 491 ; j2 = 613 ; rfact_zoom = 2. ; vcb = [0.49, 0.88, 0.49, 0.02]
+
+
+
+
+nx_res = i2-i1+1
+ny_res = j2-j1+1
+
+
+
+    
 print ' i1,i2,j1,j2 =>', i1,i2,j1,j2
 
 yx_ratio = float(ny_res)/float(nx_res)
-nxr = int(1./rfact_zoom*nx_res)
-nyr = int(1./rfact_zoom*ny_res)
+
+
+nxr = int(rfact_zoom*nx_res) ; # widt image (in pixels)
+nyr = int(rfact_zoom*ny_res) ; # height image (in pixels)
+
+dpi = 110
+
+rh  = float(nxr)/float(dpi) ; # width of figure as for figure...
+
+
+font_rat = nyr/1080.
+
+
 
 
 
@@ -84,9 +103,9 @@ if l_do_ice:
     cpal_ice = 'ncview_bw'
     vcont_ice = nmp.arange(rmin_ice, 1.05, 0.05)
 
-if cv_in == 'sosstsst':
+if cv_in in ['sosstsst','tos']:
     cfield = 'SST'
-    tmin=0. ;  tmax=25.   ;  dtemp = 1.
+    tmin=0. ;  tmax=20.   ;  dtemp = 1.
     cpal_fld = 'ncview_nrl'    
     cunit = r'SST ($^{\circ}$C)'
     cb_jump = 1
@@ -126,20 +145,20 @@ pmsk = nmp.ma.masked_where(XMSK[:,:] > 0.2, XMSK[:,:]*0.+40.)
 
 idx_oce = nmp.where(XMSK[:,:] > 0.5)
 
-
+#font_rat
 #params = { 'font.family':'Ubuntu',
 params = { 'font.family':'Helvetica Neue',
            'font.weight':    'normal',
-           'font.size':       int(22*rfact_zoom**0.2),
-           'legend.fontsize': int(22*rfact_zoom**0.2),
-           'xtick.labelsize': int(22*rfact_zoom**0.2),
-           'ytick.labelsize': int(22*rfact_zoom**0.2),
-           'axes.labelsize':  int(22*rfact_zoom**0.2) }
+           'font.size':       int(12.*font_rat),
+           'legend.fontsize': int(12.*font_rat),
+           'xtick.labelsize': int(12.*font_rat),
+           'ytick.labelsize': int(12.*font_rat),
+           'axes.labelsize':  int(12.*font_rat) }
 mpl.rcParams.update(params)
-cfont_clb  = { 'fontname':'Helvetica Neue', 'fontweight':'medium', 'fontsize':int(22*rfact_zoom**0.2), 'color':color_top }
-cfont_date = { 'fontname':'Ubuntu Mono', 'fontweight':'normal', 'fontsize':int(22*rfact_zoom**0.2), 'color':'w' }
-cfont_mail = { 'fontname':'Times New Roman', 'fontweight':'normal', 'fontstyle':'italic', 'fontsize':int(14*rfact_zoom**0.2), 'color':'0.8'}
-cfont_titl = { 'fontname':'Helvetica Neue', 'fontweight':'light', 'fontsize':int(80*rfact_zoom**0.2), 'color':'w' }
+cfont_clb  = { 'fontname':'Helvetica Neue', 'fontweight':'medium', 'fontsize':int(12.*font_rat), 'color':color_top }
+cfont_date = { 'fontname':'Ubuntu Mono', 'fontweight':'normal', 'fontsize':int(12.*font_rat), 'color':'w' }
+cfont_mail = { 'fontname':'Times New Roman', 'fontweight':'normal', 'fontstyle':'italic', 'fontsize':int(14.*font_rat), 'color':'0.8'}
+cfont_titl = { 'fontname':'Helvetica Neue', 'fontweight':'light', 'fontsize':int(30.*font_rat), 'color':'w' }
 
 
 # Colormaps for fields:
@@ -153,7 +172,7 @@ if l_do_ice:
 pal_lsm = bcm.chose_colmap('land_dark')
 norm_lsm = colors.Normalize(vmin = 0., vmax = 1., clip = False)
 
-rh = 16.*rfact_zoom
+
 
 for jt in range(jt0,Nt):
 
@@ -172,7 +191,6 @@ for jt in range(jt0,Nt):
 
     cfig = 'figs/zoom_'+cv_in+'_NEMO'+'_'+cday+'_'+chour+'.'+fig_type    
 
-    #fig = plt.figure(num = 1, figsize=(rh,rh*(9./16.)), dpi=None, facecolor='w', edgecolor='0.5')
     fig = plt.figure(num = 1, figsize=(rh,rh*yx_ratio), dpi=None, facecolor='w', edgecolor='0.5')
 
     #ax  = plt.axes([0.065, 0.05, 0.9, 1.], axisbg = '0.5')
@@ -218,13 +236,9 @@ for jt in range(jt0,Nt):
     #plt.title('NEMO: '+cfield+', coupled '+CNEMO+', '+cday+' '+chour+':00', **cfont_title)
 
 
-
     #ax2 = plt.axes([0.3, 0.08, 0.4, 0.025])
 
-    if yx_ratio > 1.:
-        ax2 = plt.axes([0.01, 0.08, 0.98, 0.025])
-    else:
-        ax2 = plt.axes([0.22, 0.08, 0.56, 0.025])
+    ax2 = plt.axes(vcb)
         
     clb = mpl.colorbar.ColorbarBase(ax2, ticks=vc_fld, cmap=pal_fld, norm=norm_fld, orientation='horizontal', extend='both')
     if cb_jump > 1:
@@ -245,20 +259,24 @@ for jt in range(jt0,Nt):
     del cf
 
 
-    #x_annot = nxr-nxr*0.22*rfact_zoom**0.2 ; y_annot = 150
+    #x_annot = nxr-nxr*0.22.*font_rat ; y_annot = 150
     x_annot = 650 ; y_annot = 1035
 
     ax.annotate('Date: '+cday+' '+chour+':00',   xy=(1, 4), xytext=(x_annot,    y_annot), **cfont_date)
 
     ax.annotate('laurent.brodeau@ocean-next.fr', xy=(1, 4), xytext=(x_annot+150, 20), **cfont_mail)
 
-    ax.annotate('NATL60', xy=(1, 4), xytext=(int(0.025*nxr), int(0.7*nyr)), **cfont_titl)
 
-    
-    plt.savefig(cfig, dpi=120, orientation='portrait', facecolor='k')
+    xl = float(nxr)/20./rfact_zoom
+    yl = float(nyr)/1.2/rfact_zoom
+    ax.annotate(CNEMO, xy=(1, 4), xytext=(xl, yl), **cfont_titl)
+
+
+    plt.savefig(cfig, dpi=dpi, orientation='portrait', facecolor='k')
     print cfig+' created!\n'
     plt.close(1)
 
 
     del cm, fig, ax, clb
+
 
