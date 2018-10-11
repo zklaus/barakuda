7c86d03e5d8f35f6da4c77ddfc85056116e34ed5
brodeau <brodeau@gmail.com>
2018-10-11 15:15:05 +0200

    LB

--- python/exec/convert_ps_to_SA.py
+++ python/exec/convert_ps_to_SA.py
@@ -54,66 +54,79 @@ if cdepth in list_var:
     print ' *** Going for accurate method cause '+cdepth+' is present in '+cf_out+' !\n'
 
 
+vcdim = f_out.variables[cv_sal].dimensions
+cv_t = vcdim[0]; print ' *** record dimension is called "'+cv_t+'"'
+Nt = f_out.dimensions[cv_t].size ; print ' *** There are '+str(Nt)+' time records...\n'
+
 # Inquire the shape of arrays:
-nb_dim = len(f_out.variables[cv_sal].dimensions)
+nb_dim = len(vcdim)
 print ' *** '+cf_out+' has '+str(nb_dim)+' dimmensions!'
-if   nb_dim==4:
-    xsal = f_out.variables[cv_sal][:,:,:,:]
-elif nb_dim==3:
-    xsal = f_out.variables[cv_sal][:,:,:]
-elif nb_dim==2:
-    xsal = f_out.variables[cv_sal][:,:]
-else:
-    print ' ERROR: unsupported number of dimmensions!' ; sys.exit(0)
 
-
+#if not nb_dim in [ 2, 3, 4 ]: print ' ERROR: unsupported number of dimmensions! =>', nb_dim ; sys.exit(0)
+if not nb_dim in [ 4 ]: print ' ERROR: unsupported number of dimmensions! =>', nb_dim ; sys.exit(0)
 
 
 if nb_dim != 4 and l_accurate:
     print '\n WARNING!!! => using less accurate method because nb_dim = '+str(nb_dim)
     print '          and we do not know how to handle this case yet....'
     l_accurate = False
+
+
+
+for jt in range(Nt):
+         
+    print '\n --- treating record # '+str(jt)
     
-
-if l_accurate:
-
-    print '\n Using accurate method with depth !'
-    
-    vz    = f_out.variables['deptht'][:]
-
-    [nt,nk,nj,ni] = nmp.shape(xsal)
-
-    print ' [nt,nk,nj,ni] =>', nt,nk,nj,ni
-    
-    xdepth = nmp.zeros((nk,nj,ni))
-
-    # building 3d arrays of depth:
-    for jk in range(nk): xdepth[jk,:,:] = vz[jk]
-
-    # pressure should be in dbar and it's the same as the depth in metre actually:
-    for jt in range(nt):
-        print ' jt =', jt
-        f_out.variables[cv_sal][jt,:,:,:] = gsw.SA_from_SP(xsal[jt,:,:,:], xdepth, -140., 0.)
-
-else:
-
-    # Fabien says it's enough:
-    if nb_dim==2:
-        f_out.variables[cv_sal][:,:]   = xsal[:,:]*SSO/35.
+    if nb_dim==4:
+        xsal = f_out.variables[cv_sal][jt,:,:,:]
+        if jt == 0:
+            (nk,nj,ni) = nmp.shape(xsal)
+        
     if nb_dim==3:
-        f_out.variables[cv_sal][:,:,:]   = xsal[:,:,:]*SSO/35.
-    elif nb_dim==4:
-        f_out.variables[cv_sal][:,:,:,:] = xsal[:,:,:,:]*SSO/35.
+        xsal = f_out.variables[cv_sal][jt,:,:]
+        if jt == 0: (nj,ni) = nmp.shape(xsal)
+        
+    if nb_dim==2:
+        xsal = f_out.variables[cv_sal][jt,:]
+        if jt == 0: (ni) = nmp.shape(xsal)
+
+    if jt == 0: shp  = nmp.shape(xsal)
+
+
+    xtmp = nmp.zeros(shp)
+
+    xtmp = xsal
+    
+    if l_accurate:
+
+        if jt == 0:
+            print '\n Using accurate method with depth !'        
+            vz    = f_out.variables['deptht'][:]
+            xdepth = nmp.zeros(shp)
+            # building 3d arrays of depth:
+            for jk in range(nk):
+                if nb_dim==4: xdepth[jk,:,:] = vz[jk]
+                if nb_dim==3: xdepth[jk,:]   = vz[jk]
+                if nb_dim==2: xdepth[jk]     = vz[jk]
+
+        # pressure should be in dbar and it's the same as the depth in metre actually:
+        if nb_dim==4: f_out.variables[cv_sal][jt,:,:,:] = gsw.SA_from_SP(xtmp[:,:,:], xdepth, -140., 0.)
+        if nb_dim==3: f_out.variables[cv_sal][jt,:,:]   = gsw.SA_from_SP(xtmp[:,:],   xdepth, -140., 0.)
+        if nb_dim==2: f_out.variables[cv_sal][jt,:]     = gsw.SA_from_SP(xtmp[:],     xdepth, -140., 0.)
+    
     else:
-        print ' WTF???'; sys.exit(1)
+    
+        # Fabien says it's enough:
+        if nb_dim==4: f_out.variables[cv_sal][jt,:,:,:] = xtmp[:,:,:]*SSO/35.
+        if nb_dim==3: f_out.variables[cv_sal][jt,:,:]   = xtmp[:,:]*SSO/35.
+        if nb_dim==2: f_out.variables[cv_sal][jt,:]     = xtmp[:]*SSO/35.
 
 
+    
+        
 f_out.variables[cv_sal].long_name = 'Absolute Salinity (TEOS10) build from practical salinity'
-
+    
 f_out.close()
-
-
-
 
 print cf_out+' sucessfully created!'
 
